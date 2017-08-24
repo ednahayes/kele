b69f3543f89a6b6c23cdaebf754df78511049065
Edna <princessahe@hotmail.com>
2017-08-24 20:34:46 +0000

    checkpoint-3-retrieve-users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,17 +1,17 @@
 require 'httparty'
-
+require 'json'
 
 class Kele
     
     include HTTParty
     
-    base_uri 'https://www.bloc.io/api/v1'
+    
     
     def initialize(email, password)
         @email = email
         @password = password
         
-        response = self.class.post('/sessions', body: {
+        response = self.class.post(api_url('sessions'), body: {
             email: email,
             password: password
         })
@@ -21,4 +21,15 @@ class Kele
     end
     
     
+    def get_me
+        response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
+        
+        @user_data = JSON.parse(response.body)  
+        
+    end   
+    
+    
+    def api_url(endpoint)
+        "https://www.bloc.io/api/v1/#{endpoint}"
+    end
 end
